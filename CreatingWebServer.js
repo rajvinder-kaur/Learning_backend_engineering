2d43e4c19108199bfe29a151f699a881f625583c
rajvinder-kaur <kaurrajvinder736@gmail.com>
2023-10-08 16:40:47 +0530

    learnt about routing and  writing headers for res

--- CreatingWebServer.js
+++ CreatingWebServer.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import  url  from 'url';
 
 
 //////////////////////////////////////////////////////////////
@@ -14,8 +15,23 @@ import http from 'http'
 //server**
 
 const server = http.createServer((req,res)=>{
-    console.log(req);
-    res.end('hello fron the server !')
+
+    const path  = req.url; //gets the url from the search bar and stores it in a variable
+
+    if(path == '/' || path == '/overview'){
+        res.end('this is an overview page !')
+    }else if(path =='/product'){
+        res.end('this is an product page !');
+    }else{
+        // write head helps to write headers for our response , 404(not found) is the status of the response sent 
+        res.writeHead(404,{
+            'Content-type':'text/html', //defined that the data i am sending in response is in the form of html 
+            'my-own-header':'hello-world' // that is a userdefined route 
+        });
+        res.end('<h1> Page not available ! </h1>'); // html type response sent to the user 
+    }
+
+    
 })
 
 // create server is used to create a server and a call back function is passed to the function that is called at each new request 
@@ -26,4 +42,4 @@ server.listen(8000, '127.0.0.1',()=>{
     console.log('====================================');
 })
 
-//we made our server work on ip 127.0.0.1 and port 8000
+//we made our server work on ip 127.0.0.1 and port 8000.
